4d83dd8bca4b6da8c143893ad83f3d52c896fb1e
krozsii <krozserd@gmail.com>
2025-05-05 16:41:55 +0200

    FibonacciController létrehozása

---
+++ BACKEND/BACKEND/Controllers/FibonacciController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BACKEND.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FibonacciController : ControllerBase
+    {
+        [HttpPost]
+        public List<int> FibonacciNumbers()
+        {
+            return new List<int>() { 1, 2, 3, 4, 5};
+        }
+    }
+}
