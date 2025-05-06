15e2a6a4b85d56294484f187793d30071373438d
krozsii <krozserd@gmail.com>
2025-05-06 12:07:17 +0200

    Fibonacci számítás

--- BACKEND/BACKEND/Data/FibonacciRepository.cs
+++ BACKEND/BACKEND/Data/FibonacciRepository.cs
@@ -6,21 +6,20 @@
         {
             List<int> fib = new List<int>();
 
-            if (iteration == 0)
-                return fib;
-
-            if(iteration == 1)
-                fib.Add(0);
-
-            if(iteration == 2)
+            for(int i = 0; i < iteration; i++)
             {
-                fib.Add(0);
-                fib.Add(1);
+                if (i == 0)
+                {
+                    fib.Add(0);
+                }
+                else
+                    if (i == 1)
+                    {
+                        fib.Add(1);
+                    }
+                else
+                    fib.Add(fib[i - 2] + fib[i-1]);
             }
-
-            if(iteration > 2)
-                for(int i = 2; fib.Count() < iteration; i++)
-                    fib.Add(fib[i-2] + fib[i-1]);
 
             return fib;
         }
