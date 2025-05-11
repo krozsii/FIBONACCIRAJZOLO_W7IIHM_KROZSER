a134568188cba4c4db76b87a950359a3249ab671
krozsii <krozserd@gmail.com>
2025-05-11 22:52:54 +0200

    Kód tisztítása

--- BACKEND/BACKEND/Program.cs
+++ BACKEND/BACKEND/Program.cs
@@ -5,14 +5,10 @@ namespace BACKEND
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
             builder.Services.AddControllers();
-
-            //builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
-
             app.UseRouting();
             app.MapControllerRoute(name: "default", pattern: "{controller}/{action=Index}/{id?}");
 
@@ -22,16 +18,11 @@ namespace BACKEND
                 app.UseSwaggerUI();
             }
 
-            //app.UseHttpsRedirection();
-            //app.UseAuthorization();
-            //app.MapControllers();
-
             app.UseCors(x => x
             .AllowCredentials()
             .AllowAnyMethod()
             .AllowAnyHeader()
             .WithOrigins("http://192.168.8.162:5500", "http://192.168.0.169:5500"));
-
 
             app.Run();
         }
--- FRONTEND/script.js
+++ FRONTEND/script.js
@@ -24,14 +24,16 @@ function sendReq() {
 }
 
 
+
+
 function drawFibonacci(numbers){
     const container = document.getElementById("spiral"); //HTML konténer, amelyben meg akarunk jeleníteni
-    container.innerHTML = "";
+    container.innerHTML = ""; //Előző rajz törlése
     const centerX = (container.offsetWidth / 2); //Konténer vízszintes közepe
     const centerY = (container.offsetHeight / 2); //Konténer függőleges közepe
 
-    const values = document.getElementById("spiral_values");
-    values.innerHTML = "";
+    const values = document.getElementById("spiral_values"); //Fibonacci sorozat számszerű megjelenítésének helye
+    values.innerHTML = ""; //Előző törlése
     
     const scale = 20; //Négyzetek nagyításának mértéke
     const directions = [
@@ -43,43 +45,43 @@ function drawFibonacci(numbers){
     let prev; //Előző négyzet eltárolásához
 
 
-    //A nullát nem tudjuk megjeleníteni
+    //Nulla kivétele a tömbből
     if(numbers.length > 0 && numbers[0] === 0){
         values.innerText = "0, ";
         numbers.shift();
     }
 
     for(let i = 0; i < numbers.length; i++){
-        const size = numbers[i] * scale;
+        const size = numbers[i] * scale; //Aktuális négyzet mérete
         let x, y;
 
-        if(i === 0){
+        if(i === 0){ //Első elem középre helyezése
             x = centerX - size / 2;
             y = centerY - size / 2;
         }
         else{
-            const dir = directions[(i - 1) % 4];
-            const prevDir = directions[(i - 1) % 4];
-            const prevSize = numbers[i - 1] * scale;
+            const dir = directions[(i - 1) % 4]; //Hosszú oldal kiválasztása
 
-            if(dir.dx === 1){
-                x = prev.x + prevSize;
-                y = prev.y + prevSize - size;
+            //Az irány függvényében kiszámolja az aktuális elem pozícióját az előző elem méretéből és helyzetéből
+            if(dir.dx === 1){ //jobb
+                x = prev.x + prev.size;
+                y = prev.y + prev.size - size;
             }
-            else if(dir.dy === -1){
-                x = prev.x + prevSize - size;
+            else if(dir.dy === -1){ //fel
+                x = prev.x + prev.size - size;
                 y = prev.y - size;
             }
-            else if(dir.dx === -1){
+            else if(dir.dx === -1){ //bal
                 x = prev.x - size;
                 y = prev.y;
             }
-            else{
+            else{ //le
                 x = prev.x;
-                y = prev.y + prevSize;
+                y = prev.y + prev.size;
             }
         }
 
+        //A HTML elem létrehozása és formázása
         const box = document.createElement("div");
         box.setAttribute("class", "box");
         box.style.width = `${size}px`;
@@ -89,8 +91,8 @@ function drawFibonacci(numbers){
         box.innerText = numbers[i];
 
         container.appendChild(box);
-        prev = {x, y, size};
+        prev = {x, y, size}; //Elmentjük az elemet, hogy a következő iterációnál ezzel tudjunk számolni
     }
 
-    values.innerText += numbers;
+    values.innerText += numbers; //Fibonacci sorozat számszerű megjelenítése
 }
