f2f37a68330211a8eca169928c01b3c65732941a
krozsii <krozserd@gmail.com>
2025-05-11 20:00:41 +0200

    Iterációk maximalizálása

--- BACKEND/BACKEND/Data/FibonacciRepository.cs
+++ BACKEND/BACKEND/Data/FibonacciRepository.cs
@@ -6,19 +6,22 @@
         {
             List<int> fib = new List<int>();
 
-            for(int i = 0; i < iteration; i++)
+            if(iteration >= 1 || iteration <= 11)
             {
-                if (i == 0)
+                for (int i = 0; i < iteration; i++)
                 {
-                    fib.Add(0);
-                }
-                else
-                    if (i == 1)
+                    if (i == 0)
+                    {
+                        fib.Add(0);
+                    }
+                    else
+                        if (i == 1)
                     {
                         fib.Add(1);
                     }
-                else
-                    fib.Add(fib[i - 2] + fib[i-1]);
+                    else
+                        fib.Add(fib[i - 2] + fib[i - 1]);
+                }
             }
 
             return fib;
