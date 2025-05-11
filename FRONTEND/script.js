4a06ae2e8d54cea285e385745648da966b531fc7
krozsii <krozserd@gmail.com>
2025-05-11 18:26:02 +0200

    Kommentek és nulla kizárása

--- FRONTEND/script.js
+++ FRONTEND/script.js
@@ -25,17 +25,24 @@ function sendReq() {
 
 
 function drawFibonacci(numbers){
-    const container = document.getElementById("spiral");
-    const centerX = (container.offsetWidth / 2);
-    const centerY = (container.offsetHeight / 2);
+    const container = document.getElementById("spiral"); //HTML konténer, amelyben meg akarunk jeleníteni
+    const centerX = (container.offsetWidth / 2); //Konténer vízszintes közepe
+    const centerY = (container.offsetHeight / 2); //Konténer függőleges közepe
     
-    const scale = 20;
+    const scale = 20; //Négyzetek nagyításának mértéke
     const dir = [
         { dx: 1, dy: 0 },   // jobbra
         { dx: 0, dy: -1 },  // fel
         { dx: -1, dy: 0 },  // balra
         { dx: 0, dy: 1 },   // le
     ];
-    let prev;
+    let prev; //Előző négyzet eltárolásához
+
+
+    //A nullát nem tudjuk megjeleníteni
+    if(numbers.length > 1 && numbers[0] === 0){
+        numbers.shift();
+    }
+
 
 }
